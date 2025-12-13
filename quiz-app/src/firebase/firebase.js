d7fb0d5354e6bd3ad941a0a8390412198f100c0a
RaviraJQuiz <ravirajtcs22@gmail.com>
2025-12-13 12:51:49 +0530

    connected firebase firestore database

--- quiz-app/src/firebase/firebase.js
+++ quiz-app/src/firebase/firebase.js
@@ -1,14 +1,16 @@
+// src/firebase/firebase.js
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyC8SWhauxU9VIaXf3LumVhwKRu7PzWcoMg",
+  authDomain: "quizapplet.firebaseapp.com",
+  projectId: "quizapplet",
+  storageBucket: "quizapplet.firebasestorage.app",
+  messagingSenderId: "207568094998",
+  appId: "1:207568094998:web:1b9fb21c60c4dd2406b237"
 };
 
 const app = initializeApp(firebaseConfig);
+
 export const db = getFirestore(app);
